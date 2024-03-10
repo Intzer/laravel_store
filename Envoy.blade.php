873a2657830ff8d8e14057681dda654148ebe289
roman <rmilh1@mail.ru>
2024-03-10 03:39:11 +0300

    Change envoy deploy file

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,9 +3,9 @@
 @task('deploy', ['on' => 'vds'])
 echo 'starting'
 cd /home/vlad/laravel_store/
-git pull
-composer update
 ./vendor/bin/sail stop
+git pull
+composer install
 ./vendor/bin/sail up -d
 ./vendor/bin/sail artisan migrate --force
 echo 'done'
