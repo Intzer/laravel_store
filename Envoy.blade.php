285b3786082c732b717f964a407bad9ec3c02a97
roman <rmilh1@mail.ru>
2024-03-10 03:07:20 +0300

    Fix Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,8 +5,8 @@ echo 'starting'
 cd /home/vlad/laravel_store/
 ./vendor/bin/sail stop
 git pull
-composer install
-./vendor/bin/sail migrate
+composer install --no-dev
+./vendor/bin/sail artisan migrate
 ./vendor/bin/sail up -d
 echo 'done'
 @endtask
